f9fad65fb1cb79733d8389df9cb02ee1bcd21149
ronaimate <ronaimate@gmail.com>
2023-05-15 21:21:05 +0200

    initial commit

---
+++ src/main/java/com/example/placeholder/rest/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.placeholder.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public ResponseEntity<String> helloWorld() {
+		return ResponseEntity.ok("Hello World!!!");
+	}
+
+}
---
+++ src/test/java/com/example/placeholder/PlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placeholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
